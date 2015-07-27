753a14cbe6d4c913d5038d670410488bbf7b3a9c
Galen Arnold <galen.arnold@asu.edu>
2015-07-27 15:56:03 -0700

    Initial commit to get files onto github. This is the start of the tip locator application.

---
+++ tipLocatorApplication.py
@@ -0,0 +1,14 @@
+'''
+This is the main run file for the tip locator application.
+This program will run an automatic routine that locates the pipette tip for
+the EHD printer in Dr. Hildreth's lab at Arizona State University.
+'''
+
+# Imports
+
+
+def tipLocatorApplication():
+    print('Application run')
+
+if __name__ == '__main__':
+    tipLocatorApplication()
