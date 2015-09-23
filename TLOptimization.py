a96f8ecf325f9b89d954985831d966b39ebfd77f
Galen Arnold <galen.arnold@asu.edu>
2015-09-23 13:43:38 -0700

    First pass of the optimization module complete and ready for testing. Also combined the two sets of test data from last week.

--- TLOptimization.py
+++ TLOptimization.py
@@ -6,7 +6,8 @@ The returned values will be the offset that the first point is from focal point 
 
 ## Imports
 # Built in modules
-
+from numpy import array, sqrt
+from scipy import optimize
 # Custom modules
 import TLParameters
 
@@ -14,23 +15,48 @@ import TLParameters
 class Optimization():
     def __init__(self):
         print('Optimization class initiated')
+        # Radius of the light entering the lens in mm
+        self.radius = 0.5
+        # Focal length of the lens in mm
+        self.focalLength = 10
+
+        # Initial guess at the solution
+        self.initialGuess = array([0.0,0.0,0.0])
 
     # Main method that will be fun
     def optimize(self):
         print('optimize method accessed')
-        sourceData, shift = self.readData()
+        # Retrieves the data points to optimize
+        sourceData = self.readData()
+        # Creates the function to be optimized
+        costFunction = self.distanceToCone(sourceData)
+
+        result = optimize.minimize(costFunction,self.initialGuess)
+
+        print(result)
 
     # Method to read the data points
     def readData(self):
         print('readData accessed')
+        # Returns the point data collected
+        return ([point for point in TLParameters.kHNSCTL_dataStorageInstances])
 
     # Method to determine the distance to the cone
-    def distanceToCone(self):
+    def distanceToCone(self,point):
         print('distanceToCone accessed')
 
+        ap = sqrt(point[0]**2 + point[2]**2)
+        b = -abs(point[1])
+
+        return (ap - (ap - self.focalLength/self.radius*b)/(1 + self.focalLength**2/self.radius**2))**2 + (b- (-self.radius/self.focalLength*ap + b)/(1+self.radius**2/self.focalLength**2))**2
+
     # Method to setup the cost function (objective function)
-    def costFunction(self):
+    def costFunction(self,data):
         print('costFunction accessed')
+
+        def cost(startingPoint):
+            return sum([self.distanceToCone(point + startingPoint) for point in data])
+        return cost
 
     # Method to record history
     def recordHistory(self):
