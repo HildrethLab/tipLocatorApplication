fc1d822a67bc3a6307b6f2c61918783fe988bb99
Galen Arnold <galen.arnold@asu.edu>
2015-10-05 13:19:16 -0700

    Optimization module is complete and ready for testing.

--- TLOptimization.py
+++ TLOptimization.py
@@ -8,6 +8,7 @@ The returned values will be the offset that the first point is from focal point 
 # Built in modules
 from numpy import array, sqrt
 from scipy import optimize
+import time
 # Custom modules
 import TLParameters
 import TLDataClass
@@ -29,8 +30,11 @@ class Optimization():
         print('optimize method accessed')
         # Retrieves the data points to optimize
         sourceData = self.readData()
+
+        # print('sourceData:')
+        # print(sourceData)
         # Creates the function to be optimized
-        costFunction = self.distanceToCone(sourceData)
+        costFunction = self.costFunction(sourceData)
 
         result = optimize.minimize(costFunction,self.initialGuess)
 
@@ -40,16 +44,22 @@ class Optimization():
     def readData(self):
         print('readData accessed')
         # Returns the point data collected
-        for i in range(len(TLParameters.kHNSCTL_dataStorageInstances)):
-            point[i]  = TLParameters.kHNSCTL_dataStorageInstances[i]
+        # Creates a blank point object
+        point = []
 
-        print(point)
+        # Cycles through the collected data and appends the coordinates to the point object
+        for i in range(len(TLParameters.kHNSCTL_dataStorageInstances)):
+            pointArray = array([TLParameters.kHNSCTL_dataStorageInstances[i].x,TLParameters.kHNSCTL_dataStorageInstances[i].y,TLParameters.kHNSCTL_dataStorageInstances[i].z])
+            point.append(pointArray)
+
+        # print(point)
+        return point
     # Method to determine the distance to the cone
     def distanceToCone(self,point):
-        print('distanceToCone accessed')
+        # print('distanceToCone accessed')
 
-        print(point[0])
-        print(type(point[0]))
+        # print(point[0])
+        # print(type(point[0]))
         ap = sqrt(point[0]**2 + point[2]**2)
         b = -abs(point[1])
 
@@ -58,7 +68,6 @@ class Optimization():
     # Method to setup the cost function (objective function)
     def costFunction(self,data):
         print('costFunction accessed')
-
         def cost(startingPoint):
             return sum([self.distanceToCone(point + startingPoint) for point in data])
         return cost
@@ -69,25 +78,26 @@ class Optimization():
 
 def main():
     # Loads test data with a known solution
-    ######
-    # Known solutons: -0.22888566,  0.24425689, -0.9640246
-    #####
-    TLDataClass.TLData(0.13736903, -2.09331181,  1.176831, 5)
-    TLDataClass.TLData(0.19324891, -2.23147539,  1.20972186, 5)
-    TLDataClass.TLData(0.35961296, -3.67184001,  1.36968941, 5)
-    TLDataClass.TLData(0.38395479, -2.74271983,  1.23765356, 5)
-    TLDataClass.TLData(0.56420493, -3.09959046,  1.08545504, 5)
-    TLDataClass.TLData(-0.1346031 , -3.68954062,  1.19486776, 5)
+    # ######
+    # # Known solutons: -0.22888566,  0.24425689, -0.9640246
+    # # The code works with this solution
+    # #####
+    # TLDataClass.TLData(0.13736903, -2.09331181,  1.176831, 5)
+    # TLDataClass.TLData(0.19324891, -2.23147539,  1.20972186, 5)
+    # TLDataClass.TLData(0.35961296, -3.67184001,  1.36968941, 5)
+    # TLDataClass.TLData(0.38395479, -2.74271983,  1.23765356, 5)
+    # TLDataClass.TLData(0.56420493, -3.09959046,  1.08545504, 5)
+    # TLDataClass.TLData(-0.1346031 , -3.68954062,  1.19486776, 5)
+    #
+    # TLDataClass.TLData(0.33198168, -3.22328235,  0.31439186, 5)
+    # TLDataClass.TLData(0.06276181, -3.60649323,  0.14596918, 5)
+    # TLDataClass.TLData(0.85432262, -3.14077231,  0.15677479, 5)
+    # TLDataClass.TLData(0.86477496, -3.5339717 ,  0.22202918, 5)
+    # TLDataClass.TLData(2.94188149e-02,  -3.43052042e+00,  -3.14398441e-03, 5)
+    # TLDataClass.TLData(0.50373139, -1.868233  ,  0.18008235, 5)
 
-    point = []
-
-    for i in range(len(TLParameters.kHNSCTL_dataStorageInstances)):
-        point.append(TLParameters.kHNSCTL_dataStorageInstances[i])
-
-    print(point)
-
-    # optimizationTest = Optimization()
-    # optimizationTest.optimize()
+    optimizationTest = Optimization()
+    optimizationTest.optimize()
 
 if __name__ == '__main__':
     main()
