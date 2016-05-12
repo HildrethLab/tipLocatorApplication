5c2253211543a7eb665a634af29c9e9dcbfddce8
Equipment <equipment@en4100919.dhcp.asu.edu>
2016-05-12 12:44:57 -0700

    Changes made to show Chris how the GUI creation works.

--- buildPythonUIFile.sh
+++ buildPythonUIFile.sh
@@ -1,5 +1,6 @@
 echo “This is a simple shell script that is used to update the base UI python file when changes are made in QT Designer”
 
-cd /Users/galenarnold/Documents/Galen/Python/tipLocatorApplication
-pyuic4 tipLocatorUIBase.ui > tipLocatorUIBase.py
+cd /Users/equipment/Documents/Code/tipLocatorApplication
+pyuic5 tipLocatorUIBase.ui > tipLocatorUIBase.py
 osascript -e 'tell application "Terminal" to quit'
+
